915d14e6ca262aba63ae1ac603db77b5622f07e9
Sathwick <sathwick.p7@gmail.com>
2023-06-11 22:13:57 +0530

    Multi-Touch Supported

--- Assets/Files/Scripts/ARDrawManager.cs
+++ Assets/Files/Scripts/ARDrawManager.cs
@@ -7,177 +7,114 @@ using UnityEngine.XR.ARFoundation;
 [RequireComponent(typeof(ARAnchorManager))]
 public class ARDrawManager : Singleton<ARDrawManager>
 {
-    [SerializeField] private float distanceFromCamera = 0.3f;
-    private string tagg = "Line";
-    [SerializeField] private Material defaultColorMaterial;
+    [SerializeField]
+    private LineSettings lineSettings = null;
 
-    [SerializeField] private int cornerVertices = 5;
-    [SerializeField] private int endCapVertices = 5;
+    [SerializeField]
+    private UnityEvent OnDraw = null;
 
-    [Header("Tolerance Options")][SerializeField]
-    private bool allowSimplification = false;
+    [SerializeField]
+    private ARAnchorManager anchorManager = null;
 
-    [SerializeField] private float tolerance = 0.001f;
+    [SerializeField] 
+    private Camera arCamera = null;
 
-    [SerializeField] private float applySimplifyAfterPoints = 20.0f;
-
-    [SerializeField, Range(0,1.0f)]
-    private float minDistanceBeforeNewPoint = 0.01f;
-
-    [SerializeField] private UnityEvent OnDraw;
-    [SerializeField] private ARAnchorManager anchorManager;
-
-    [SerializeField] private Camera arCamera;
-
-    [SerializeField] private Color defaultColor = Color.white;
-
-    [SerializeField] private float lineWidth = 0.05f;
-
-    private LineRenderer prevLineRender;
-    private LineRenderer currentLineRender;
     private List<ARAnchor> anchors = new List<ARAnchor>();
-    private List<LineRenderer> lines = new List<LineRenderer>();
 
-    private int positionCount = 0;
+    private Dictionary<int, ARLine> Lines = new Dictionary<int, ARLine>();
 
-    private Vector3 prevPointdistance = Vector3.zero;
+    private bool CanDraw { get; set; }
 
-    private bool CanDraw {get; set;}
-
-    void Update()
+    void Update ()
     {
-        #if !UNITY_EDITOR
-        if (Input.touchCount > 0)
-            DrawOnTouch();
+        #if !UNITY_EDITOR    
+        DrawOnTouch();
         #else
-        if(Input.GetMouseButton(0))
-            DrawOnMouse();
-        else{
-            prevLineRender = null;
-        }
+        DrawOnMouse();
         #endif
-    }
+	}
 
-    public void AllowDraw(bool isAllow){
+    public void AllowDraw(bool isAllow)
+    {
         CanDraw = isAllow;
     }
 
-    private void SetLineSettings(LineRenderer currentLineRenderer){
-        currentLineRender.startWidth = lineWidth;
-        currentLineRender.endWidth = lineWidth;
-        currentLineRender.numCornerVertices = cornerVertices;
-        currentLineRender.numCapVertices = endCapVertices;
 
-        if(allowSimplification) currentLineRender.Simplify(tolerance);
-
-        currentLineRender.startColor = defaultColor;
-        currentLineRender.endColor = defaultColor;
-    }
-
-    void DrawOnTouch(){
+    void DrawOnTouch()
+    {
         if(!CanDraw) return;
 
-        Touch touch = Input.GetTouch(0);
+        int tapCount = Input.touchCount > 1 && lineSettings.allowMultiTouch ? Input.touchCount : 1;
 
-        Vector3 touchPosition = arCamera.ScreenToWorldPoint(new Vector3(Input.GetTouch(0).position.x, Input.GetTouch(0).position.y, distanceFromCamera));
-        if(touch.phase == TouchPhase.Began)
+        for(int i = 0; i < tapCount; i++)
+        {
+            Touch touch = Input.GetTouch(i);
+            Vector3 touchPosition = arCamera.ScreenToWorldPoint(new Vector3(Input.GetTouch(i).position.x, Input.GetTouch(i).position.y, lineSettings.distanceFromCamera));
+            
+            ARDebugManager.Instance.LogInfo($"{touch.fingerId}");
+
+            if(touch.phase == TouchPhase.Began)
+            {
+                OnDraw?.Invoke();
+                
+                ARAnchor anchor = anchorManager.AddAnchor(new Pose(touchPosition, Quaternion.identity));
+                if (anchor == null) 
+                    Debug.LogError("Error creating reference point");
+                else 
+                {
+                    anchors.Add(anchor);
+                    ARDebugManager.Instance.LogInfo($"Anchor created & total of {anchors.Count} anchor(s)");
+                }
+
+                ARLine line = new ARLine(lineSettings);
+                Lines.Add(touch.fingerId, line);
+                line.AddNewLineRenderer(transform, anchor, touchPosition);
+            }
+            else if(touch.phase == TouchPhase.Moved || touch.phase == TouchPhase.Stationary)
+            {
+                Lines[touch.fingerId].AddPoint(touchPosition);
+            }
+            else if(touch.phase == TouchPhase.Ended)
+            {
+                Lines.Remove(touch.fingerId);
+            }
+        }
+    }
+
+    void DrawOnMouse()
+    {
+        if(!CanDraw) return;
+
+        Vector3 mousePosition = arCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, lineSettings.distanceFromCamera));
+
+        if(Input.GetMouseButton(0))
         {
             OnDraw?.Invoke();
 
-            ARAnchor anchor = anchorManager.AddAnchor(new Pose(touchPosition, Quaternion.identity));
-
-            if(anchor == null){
-                Debug.Log("error creating refernce point");
-
+            if(Lines.Keys.Count == 0)
+            {
+                ARLine line = new ARLine(lineSettings);
+                Lines.Add(0, line);
+                line.AddNewLineRenderer(transform, null, mousePosition);
             }
-            else{
-                anchors.Add(anchor);
-                ARDebugManager.Instance.LogInfo($"Anchor created and total of {anchors.Count} anchor(s)");
-
-            }
-            AddNewLineRenderer(anchor,touchPosition);
-        }
-        else{
-            UpdateLine(touchPosition);
-        }
-    }
-
-
-    void DrawOnMouse(){
-        if(!CanDraw) return;
-
-        Vector3 mousePosition = arCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x,Input.mousePosition.y, distanceFromCamera));
-
-        if(Input.GetMouseButton(0)){
-            OnDraw?.Invoke();
-            if(prevLineRender == null){
-                AddNewLineRenderer(null,mousePosition);
-            }
-            else{
-                UpdateLine(mousePosition);
+            else 
+            {
+                Lines[0].AddPoint(mousePosition);
             }
         }
-    }
-
-
-    void UpdateLine(Vector3 touchPosition){
-        if(prevPointdistance == null){
-            prevPointdistance = touchPosition;
-        }
-        if(prevPointdistance != null && Mathf.Abs(Vector3.Distance(prevPointdistance, touchPosition)) >= minDistanceBeforeNewPoint){
-            prevPointdistance = touchPosition;
-            Addpoint(prevPointdistance);
+        else if(Input.GetMouseButtonUp(0))
+        {
+            Lines.Remove(0);   
         }
     }
-    void Addpoint(Vector3 position){
 
-        positionCount++;
-
-        currentLineRender.positionCount = positionCount;
-
-        currentLineRender.SetPosition(positionCount-1,position);
-
-        if(currentLineRender.positionCount % applySimplifyAfterPoints == 0 && allowSimplification){
-            currentLineRender.Simplify(tolerance);
-        }
-    }
-    void AddNewLineRenderer(ARAnchor arAnchor, Vector3 touchPostion){
-        
-        positionCount = 2;
-        GameObject go = new GameObject($"LineRenderer_{lines.Count}");
-        // go.tag = "Line";
-
-        go.transform.parent = arAnchor?.transform ?? transform;
-        go.transform.position = touchPostion;
-        go.gameObject.tag = "Line";
-        LineRenderer goLineRenderer = go.AddComponent<LineRenderer>();
-        // goLineRenderer.tag = tagg;
-        goLineRenderer.startWidth = lineWidth;
-        goLineRenderer.endWidth = lineWidth;
-        goLineRenderer.material =defaultColorMaterial;
-        goLineRenderer.useWorldSpace = true;
-        goLineRenderer.positionCount = positionCount;
-        goLineRenderer.numCapVertices = 90;
-        goLineRenderer.SetPosition(0, touchPostion);
-        goLineRenderer.SetPosition(1, touchPostion);
-
-
-        currentLineRender = goLineRenderer;
-        SetLineSettings(goLineRenderer);
-
-        prevLineRender = currentLineRender;
-
-        lines.Add(goLineRenderer);
-
-        ARDebugManager.Instance.LogInfo($"New line renderer created");
-
-
-    }
-    GameObject[] GetAllLinesInScene(){
+    GameObject[] GetAllLinesInScene()
+    {
         return GameObject.FindGameObjectsWithTag("Line");
     }
 
-    public void ClearLines(){
+    public void ClearLines()
+    {
         GameObject[] lines = GetAllLinesInScene();
         foreach (GameObject currentLine in lines)
         {
@@ -185,7 +122,4 @@ public class ARDrawManager : Singleton<ARDrawManager>
             Destroy(currentLine);
         }
     }
-
-    private Color GetRandomColor() => Random.ColorHSV(0f,1f,1f,1f,0.5f,1f);
-
 }
